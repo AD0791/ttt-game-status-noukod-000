bee0d2e54310a32e0f1c6a9b2cd1f33deb731985
Alexandro Disla <alexandrodisla@hotmail.com>
2019-04-09 02:08:36 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -15,19 +15,11 @@ WIN_COMBINATIONS = [
   [6,4,2]
 ]
 
-def won? board
-  WIN_COMBINATIONS.each do |c|
-    winpo = c[0]
-    winpo2 = c[1]
-    winpo3 = c[2]
-    
-    if board[winpo] == "X"&&board[winpo2]=="X"&&board[winpo3]=="X"
-      return c
-    elsif board[winpo] == "O"&&board[winpo2]=="O"&&board[winpo3] == "O"
-      return c
-    else
-      false
-    end
+def won?(board)
+  WIN_COMBINATIONS.detect do |combo|
+    board[combo[0]] == board[combo[1]] &&
+    board[combo[1]] == board[combo[2]] &&
+    position_taken?(board, combo[0])
   end
 end
 
