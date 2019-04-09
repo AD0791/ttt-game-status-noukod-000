0415f59e4bc79b44bc507ed9d92b4c0ab94a05da
Alexandro Disla <alexandrodisla@hotmail.com>
2019-04-09 01:30:04 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -24,6 +24,7 @@ def won? board
     if board[winpo] = "X"&&board[winpo2]=="X"&&board[winpo3]=="X"
       return c
     elsif board[winpo] = "O"&&board[winpo2]=="O"&&board[winpo3] == "O"
+      return c
     else
       position_taken?(board,c)
     end
